163c8fd9136640c645b7d0d93ec6914ba2103380
Sean-300 <sstclair@uvm.edu>
2023-03-08 20:25:51 -0500

    Adding comments to test cases

--- test_cases.cpp
+++ test_cases.cpp
@@ -9,19 +9,19 @@ using namespace std;
  * Effects: Returns whether or not the given string is a valid moo,
  * based on the following conditions:
  *
- * 0  - A moo is not the empty string
- * 1  - Starts with an m
- * 2  - Ends with an o
- * 3  - Contains at least 2 o's
- * 4  - No numbers
- * 5  - No special characters
- * 6  - If the M is capital, the rest can be any case, otherwise must all be lowercase
- * 7  - Even number of o's
- * 8  - Capital o's have to precede lowercase o's
- * 9  - Even number of capital o's
- * 10 - All m's have to occur at the beginning
- * 11 - Either all lowercase or all capital m's
- * 12 - Strictly odd-number of m's
+ * 1  - A moo is not the empty string
+ * 2  - Starts with an m
+ * 3  - Ends with an o
+ * 4  - Contains at least 2 o's
+ * 5  - No numbers
+ * 6  - No special characters
+ * 7  - If the M is capital, the rest can be any case, otherwise must all be lowercase
+ * 8  - Even number of o's
+ * 9  - Capital o's have to precede lowercase o's
+ * 10  - Even number of capital o's
+ * 11 - All m's have to occur at the beginning
+ * 12 - Either all lowercase or all capital m's
+ * 13 - Strictly odd-number of m's
  */
 bool mooIsValid(string moo);
 
@@ -37,10 +37,19 @@ int main() {
 bool test_moo() {
     bool passed = true;
 
-    if (!mooIsValid("oo")) {
-        cout << "Failed Moo test case" << endl;
-        passed = false;
-    }
+    // 1 - A moo is not the empty string
+    // 2 - Starts with an m
+    // 3 - Ends with an o
+    // 4 - Contains at least 2 o's
+    // 5 - No numbers
+    // 6 - No special characters
+    // 7 - If the M is capital, the rest can be any case, otherwise must all be lowercase
+    // 8 - Even number of o's
+    // 9 - Capital o's have to precede lowercase o's
+    // 10 - Even number of capital o's
+    // 11 - All m's have to occur at the beginning
+    // 12 - Either all lowercase or all capital m's
+    // 13 - Strictly odd-number of m's
 
     return passed;
 }
