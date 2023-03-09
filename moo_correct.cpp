2e38ef046cde1d93f5a27bc10738c4e87fb00dda
Sean-300 <sstclair@uvm.edu>
2023-03-08 20:40:10 -0500

    Added odd-numbered test cases in test_cases.cpp

--- moo_correct.cpp
+++ moo_correct.cpp
@@ -30,26 +30,26 @@ bool mooIsValid(string moo) {
         }
     }
 
-    // 1  - A moo is not the empty string
+    // 0  - A moo is not the empty string
     if (moo == "") {
         valid = false;
     }
 
-    // 2  - A valid moo starts with an m
+    // 1  - A valid moo starts with an m
     if (tolower(*moo.begin()) != 'm')
         valid = false;
 
-    // 3  - Ends with an o
+    // 2  - Ends with an o
     if (tolower(*moo.end()) != 'o') {
         valid = false;
     }
 
-    // 4  - Contains at least 2 o's
+    // 3  - Contains at least 2 o's
     if (numOs < 2) {
         valid = false;
     }
 
-    // 5  - No numbers
+    // 4  - No numbers
     for (char c: moo) {
         if (isdigit(c)) {
             valid = false;
@@ -57,7 +57,7 @@ bool mooIsValid(string moo) {
         }
     }
 
-    // 6  - No special characters
+    // 5  - No special characters
     for (char c: moo) {
         if (!isalpha(c)) {
             valid = false;
@@ -65,17 +65,17 @@ bool mooIsValid(string moo) {
         }
     }
 
-    // 7  - If the M is capital, the rest can be any case, otherwise must all be lowercase
+    // 6  - If the M is capital, the rest can be any case, otherwise must all be lowercase
     if (moo != lowerMoo) {
         valid = false;
     }
 
-    // 8  - Even number of o's
+    // 7  - Even number of o's
     if (numOs % 2 != 0) {
         valid = false;
     }
 
-    // 9  - Capital o's have to precede lowercase o's
+    // 8  - Capital o's have to precede lowercase o's
     int lowerOIndex;
     if (moo.find('o') != string::npos) {
         lowerOIndex = moo.find('o');
@@ -84,12 +84,12 @@ bool mooIsValid(string moo) {
         }
     }
 
-    // 10  - Even number of capital o's
+    // 9  - Even number of capital o's
     if (numOsCapital % 2 != 0) {
         valid = false;
     }
 
-    // 11 - All m's have to occur at the beginning
+    // 10 - All m's have to occur at the beginning
     if (lowerMoo.find('o') != string::npos) {
         lowerOIndex = lowerMoo.find('o');
         if (lowerMoo.find('m', lowerOIndex) != string::npos) {
@@ -97,14 +97,14 @@ bool mooIsValid(string moo) {
         }
     }
 
-    // 12 - Either all lowercase or all capital m's
+    // 11 - Either all lowercase or all capital m's
     if (numMsCapital != numMs) {
         if (numMsCapital != 0) {
             valid = false;
         }
     }
 
-    // 13 - Strictly odd-number of m's
+    // 12 - Strictly odd-number of m's
     if (numMs % 2 == 0) {
         valid = false;
     }
--- test_cases.cpp
+++ test_cases.cpp
@@ -9,19 +9,19 @@ using namespace std;
  * Effects: Returns whether or not the given string is a valid moo,
  * based on the following conditions:
  *
- * 1  - A moo is not the empty string
- * 2  - Starts with an m
- * 3  - Ends with an o
- * 4  - Contains at least 2 o's
- * 5  - No numbers
- * 6  - No special characters
- * 7  - If the M is capital, the rest can be any case, otherwise must all be lowercase
- * 8  - Even number of o's
- * 9  - Capital o's have to precede lowercase o's
- * 10  - Even number of capital o's
- * 11 - All m's have to occur at the beginning
- * 12 - Either all lowercase or all capital m's
- * 13 - Strictly odd-number of m's
+ * 0  - A moo is not the empty string
+ * 1  - Starts with an m
+ * 2  - Ends with an o
+ * 3  - Contains at least 2 o's
+ * 4  - No numbers
+ * 5  - No special characters
+ * 6  - If the M is capital, the rest can be any case, otherwise must all be lowercase
+ * 7  - Even number of o's
+ * 8  - Capital o's have to precede lowercase o's
+ * 9  - Even number of capital o's
+ * 10 - All m's have to occur at the beginning
+ * 11 - Either all lowercase or all capital m's
+ * 12 - Strictly odd-number of m's
  */
 bool mooIsValid(string moo);
 
@@ -29,7 +29,7 @@ bool test_moo();
 
 int main() {
     if (test_moo()) {
-        cout << "Passed all test cases!" << endl;
+        cout << "PASSED all test cases!" << endl;
     }
     return 0;
 }
@@ -37,19 +37,49 @@ int main() {
 bool test_moo() {
     bool passed = true;
 
-    // 1 - A moo is not the empty string
-    // 2 - Starts with an m
-    // 3 - Ends with an o
-    // 4 - Contains at least 2 o's
-    // 5 - No numbers
-    // 6 - No special characters
-    // 7 - If the M is capital, the rest can be any case, otherwise must all be lowercase
-    // 8 - Even number of o's
-    // 9 - Capital o's have to precede lowercase o's
-    // 10 - Even number of capital o's
-    // 11 - All m's have to occur at the beginning
-    // 12 - Either all lowercase or all capital m's
-    // 13 - Strictly odd-number of m's
+    // 0 - A moo is not the empty string
+    // 1 - Starts with an m
+    if (mooIsValid("noo")) {
+        cout << "FAILED 'Starts with an m' test case" << endl;
+        passed = false;
+    }
+
+    // 2 - Ends with an o
+    // 3 - Contains at least 2 o's
+    if (mooIsValid("mo")) {
+        cout << "FAILED 'Contains at least 2 o's' test case" << endl;
+        passed = false;
+    }
+
+    // 4 - No numbers
+    // 5 - No special characters
+    if (mooIsValid("$moo")) {
+        cout << "FAILED 'No special characters' test case" << endl;
+        passed = false;
+    }
+
+    // 6 - If the M is capital, the rest can be any case, otherwise must all be lowercase
+    // 7 - Even number of o's
+    if (mooIsValid("mooo")) {
+        cout << "FAILED 'Even number of o's' test case" << endl;
+        passed = false;
+    }
+
+    // 8 - Capital o's have to precede lowercase o's
+    // 9 - Even number of capital o's
+    if (mooIsValid("MOoo")) {
+        cout << "FAILED 'Even number of o's' test case" << endl;
+        passed = false;
+    }
+
+    // 10 - All m's have to occur at the beginning
+    // 11 - Either all lowercase or all capital m's
+    if (mooIsValid("Mmoo")) {
+        cout << "FAILED 'Either all lowercase or all capital m's' test case 1" << endl;
+        passed = false;
+    }
+
+    // 12 - Strictly odd-number of m's
 
     return passed;
 }
