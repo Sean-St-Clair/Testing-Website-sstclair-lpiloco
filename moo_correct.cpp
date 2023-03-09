c0ba751f5e97456ca8351a52333629e9cd68cbe6
Sean-300 <sstclair@uvm.edu>
2023-03-08 19:40:45 -0500

    Added 3rd condition in mooIsValid

--- moo_correct.cpp
+++ moo_correct.cpp
@@ -15,11 +15,23 @@ bool mooIsValid(string moo) {
         lowerMoo += tolower(c);
 
     // 0  - A moo is not the empty string
+
     // 1  - A valid moo starts with an m
     if (lowerMoo[0] != 'm')
         valid = false;
+
     // 2  - Ends with an o
+
     // 3  - Contains at least 2 o's
+    int numOs;
+    for (char c: lowerMoo) {
+        if (c == 'o') {
+            numOs++;
+        }
+    }
+    if (numOs < 2)
+        valid = false;
+
     // 4  - No numbers
 
     // 5  - No special characters
@@ -29,6 +41,7 @@ bool mooIsValid(string moo) {
             break;
         }
     }
+
     // 6  - If the M is capital, the rest can be any case, otherwise must all be lowercase
     // 7  - Even number of o's
     // 8  - Capital o's have to precede lowercase o's
